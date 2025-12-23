c21af1a1db3449eaf42866c18b7e9156b7a52eba
Amir Azizov <creator005@mail.ru>
2025-12-23 16:28:28 +0300

    empty

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,5 @@
-import { StrictMode } from 'react'
 import { CssBaseline, GlobalStyles } from '@mui/material'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
